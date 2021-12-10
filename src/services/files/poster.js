b69a1942180bc4a57a527c5016259f1117c5c0f4
aizada869-nazarova <83946225+aizada869-nazarova@users.noreply.github.com>
2021-12-10 15:31:25 +0000

    add changes

--- src/services/files/poster.js
+++ src/services/files/poster.js
@@ -2,6 +2,10 @@ import express from 'express'
 import multer from 'multer'
 import createHttpError from 'http-errors'
 import { getMedia, writeMedia, saveMediaPoster } from "../../lib/fs-tools.js"
+// import path from "path"
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
+
 
 
 
@@ -12,21 +16,45 @@ const uploader = multer({
         if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/gif" && file.mimetype !== "image/png") {
             multerNext(createHttpError(400, "File type not supported: please try with a jpeg, gif or png."))
         } else if (file.size > 3000000) {
-            multerNext(createHttpError(400, "The image is too large: please upload an image under 3MB."))
+            multerNext(createHttpError(400, "The image is too large."))
         } else {
             multerNext(null, true)
         }
     }
 }).single("mediaPoster")
 
+// const cloudinaryStorage = new CloudinaryStorage({
+//     cloudinary,
+//     params:{
+//     folder:"medias"
+//     }
+// })
+
+// const uploadOnCloudinary = multer({ storage: cloudinaryStorage}).single("mediaCover")
+
+
 posterRouter.post("/uploadSingle", uploader, async (req, res, next) => {
    
     try {
-      console.log("FILE: ", req.file)
-      await saveMediaPoster (req.file.originalname, req.file.buffer)
-     
-      res.send("OK")
-    } catch (error) {
+
+    //   const mediaList = await getMedia()
+    //   const mediaIndex = mediaList.findIndex(media => media.id === req.params.mediaId)
+    //   const fileName = `${ req.params.mediaId }.${ req.file.mimetype.slice(6) }`
+    //   await saveMediaPoster (req.file.originalname, req.file.buffer)
+    //   mediaList[mediaIndex].poster = `http://localhost:3001/poster/media ${ fileName }`
+    //     await writeMedia(mediaList)
+    //     res.send("uploaded")     
+    const newMedia = {mediaCover: req.file.path}
+    const url = newMedia.mediaCover
+    const medias = await getMedia()
+    const media = medias.find(media => media._imdbID === req.params.id)
+    if(media){
+        media.Poster = url
+        await writeMedia(medias)
+    }
+    res.send(url)
+
+ } catch (error) {
       next(error)
     }
   })
